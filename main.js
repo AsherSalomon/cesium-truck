473b9c9188f748680dc102eb7fce0378f05085de
AsherSalomon <AsherSalomon@gmail.com>
2023-01-07 08:22:12 -0500

    Update main.js

--- main.js
+++ main.js
@@ -4,9 +4,27 @@
 Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI1ZGI0ZWQ4Ny0zNjNjLTRmZDAtYWI2YS0yNTc0N2ViNjY0OTEiLCJpZCI6MTAwNDY2LCJpYXQiOjE2NzMwOTcwNjl9.7118r-iVka0QWiKRgKK2u46jqyibkOwYaewwdnFlHUU';
 
 // Initialize the Cesium Viewer in the HTML element with the `cesiumContainer` ID.
-const viewer = new Cesium.Viewer('cesiumContainer', {
-  terrainProvider: Cesium.createWorldTerrain()
-});    
+viewer = new Cesium.Viewer('cesiumContainer', {
+  terrainProvider: Cesium.createWorldTerrain({
+    requestWaterMask: true,
+    requestVertexNormals: true,
+  }),
+  geocoder: false,
+  homeButton: false,
+  sceneModePicker: false,
+  baseLayerPicker: false,
+  navigationHelpButton: false,
+  animation: false,
+  timeline: false,
+  fullscreenButton: false,
+});
+
+viewer.clock.clockStep = Cesium.ClockStep.SYSTEM_CLOCK;
+viewer.scene.globe.enableLighting = true;
+viewer.shadows = true;
+viewer.scene.globe.depthTestAgainstTerrain = true;
+viewer.scene.moon = new Cesium.Moon();
+
 // Add Cesium OSM Buildings, a global 3D buildings layer.
 const buildingTileset = viewer.scene.primitives.add(Cesium.createOsmBuildings());   
 // Fly the camera to San Francisco at the given longitude, latitude, and height.
