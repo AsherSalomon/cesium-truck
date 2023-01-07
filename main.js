9ffda03d23855513c8d1a33c32d4950012b83653
AsherSalomon <AsherSalomon@gmail.com>
2023-01-07 18:11:22 -0500

    Update main.js

--- main.js
+++ main.js
@@ -25,11 +25,6 @@ viewer.shadows = true;
 viewer.scene.globe.depthTestAgainstTerrain = true;
 viewer.scene.moon = new Cesium.Moon();
 
-viewer.camera.frustum = new Cesium.PerspectiveFrustum({
-    fov : Cesium.Math.PI_OVER_THREE,
-    aspectRatio : 1
-});
-
 // Add Cesium OSM Buildings, a global 3D buildings layer.
 const buildingTileset = viewer.scene.primitives.add(Cesium.createOsmBuildings());   
 // Fly the camera to San Francisco at the given longitude, latitude, and height.
