50fc951a1c13d9fb54789c011a82da3279191a59
AsherSalomon <AsherSalomon@gmail.com>
2023-01-08 07:05:23 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
+const truckEntities = [];
+
 // Your access token can be found at: https://ion.cesium.com/tokens.
 // Replace `your_access_token` with your Cesium ion access token.
-
 Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI1ZGI0ZWQ4Ny0zNjNjLTRmZDAtYWI2YS0yNTc0N2ViNjY0OTEiLCJpZCI6MTAwNDY2LCJpYXQiOjE2NzMwOTcwNjl9.7118r-iVka0QWiKRgKK2u46jqyibkOwYaewwdnFlHUU';
 
 // Initialize the Cesium Viewer in the HTML element with the `cesiumContainer` ID.
@@ -36,3 +37,25 @@ viewer.camera.flyTo({
   }
 });
 
+truckEntities[0] = createModel('1984_Ford_F350.glb');
+for (let i = 1; i <= 4; i++) {
+  truckEntities[i] = viewer.entities.add({model: {uri: '1984_Ford_F350_wheel.glb'}});
+}
+
+window.addEventListener('keydown', function(e) {
+  followTruck = true;
+  if (e.keyCode == 69) {
+    if (viewer.trackedEntity == truckEntities[0]) {
+      viewer.trackedEntity = null;
+    } else if (viewer.trackedEntity != truckEntities[0]) {
+      const matrix4 = new Cesium.Matrix4();
+      Cesium.Transforms.eastNorthUpToFixedFrame(
+        truckEntities[0].position._value, Cesium.Ellipsoid.WGS84, matrix4);
+      Cesium.Matrix4.inverse(matrix4, matrix4);
+      const position = new Cesium.Cartesian3();
+      Cesium.Matrix4.multiplyByPoint(matrix4, viewer.camera.position, position);
+      truckEntities[0].viewFrom = position;
+      viewer.trackedEntity = truckEntities[0];
+    }
+  }
+});
