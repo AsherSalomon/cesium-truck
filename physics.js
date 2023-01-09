2e26a5ed0c8a113ea4913b2b656f49c50606e1dd
AsherSalomon <AsherSalomon@gmail.com>
2023-01-09 18:32:06 -0500

    Update physics.js

--- physics.js
+++ physics.js
@@ -58,45 +58,45 @@ export function init(newTruck, newViewer) {
 let frameCount = 0;
 let previousTruckSelected = false;
 export function update(delta) {
-  const truckSelected = viewer.trackedEntity == truckEntities[0];
-  if (truckSelected != previousTruckSelected) {
-    const position = truckEntities[0].position._value;
-    const offset = new Cesium.Cartesian3();
-    Cesium.Cartesian3.subtract(position, originOffset, offset);
-    const btPosition = new Ammo.btVector3(offset.x, offset.y, offset.z);
-    const tm = vehicle.getChassisWorldTransform();
-    tm.setOrigin(btPosition);
+//   const truckSelected = viewer.trackedEntity == truckEntities[0];
+//   if (truckSelected != previousTruckSelected) {
+//     const position = truckEntities[0].position._value;
+//     const offset = new Cesium.Cartesian3();
+//     Cesium.Cartesian3.subtract(position, originOffset, offset);
+//     const btPosition = new Ammo.btVector3(offset.x, offset.y, offset.z);
+//     const tm = vehicle.getChassisWorldTransform();
+//     tm.setOrigin(btPosition);
 
-    const orient = truckEntities[0].orientation._value.clone();
-    const quatB = new Cesium.Quaternion(0, 0, 0, 1);
-    Cesium.Quaternion.fromAxisAngle(Cesium.Cartesian3.UNIT_X, Math.PI / 2, quatB);
-    Cesium.Quaternion.multiply(orient, quatB, orient);
-    Cesium.Quaternion.fromAxisAngle(Cesium.Cartesian3.UNIT_Y, Math.PI, quatB);
-    Cesium.Quaternion.multiply(orient, quatB, orient);
-    tm.setRotation(new Ammo.btQuaternion(orient.x, orient.y, orient.z, orient.w));
+//     const orient = truckEntities[0].orientation._value.clone();
+//     const quatB = new Cesium.Quaternion(0, 0, 0, 1);
+//     Cesium.Quaternion.fromAxisAngle(Cesium.Cartesian3.UNIT_X, Math.PI / 2, quatB);
+//     Cesium.Quaternion.multiply(orient, quatB, orient);
+//     Cesium.Quaternion.fromAxisAngle(Cesium.Cartesian3.UNIT_Y, Math.PI, quatB);
+//     Cesium.Quaternion.multiply(orient, quatB, orient);
+//     tm.setRotation(new Ammo.btQuaternion(orient.x, orient.y, orient.z, orient.w));
 
-    const body = vehicle.getRigidBody();
-    body.setLinearVelocity(new Ammo.btVector3(0, 0, 0));
-    body.setAngularVelocity(new Ammo.btVector3(0, 0, 0));
-  }
-  if (truckSelected) {
-    if (gravityOn) {
-      const position = truckEntities[0].position.getValue(truckEntities.now());
-      const normal = new Ammo.btVector3(position.x, position.y, position.z);
-      normal.normalize();
-      normal.op_mul(-gravity);
-      physicsWorld.setGravity( normal );
-    } else {
-      physicsWorld.setGravity( new Ammo.btVector3(0, 0, 0) );
-    }
+//     const body = vehicle.getRigidBody();
+//     body.setLinearVelocity(new Ammo.btVector3(0, 0, 0));
+//     body.setAngularVelocity(new Ammo.btVector3(0, 0, 0));
+//   }
+//   if (truckSelected) {
+//     if (gravityOn) {
+//       const position = truckEntities[0].position.getValue(truckEntities.now());
+//       const normal = new Ammo.btVector3(position.x, position.y, position.z);
+//       normal.normalize();
+//       normal.op_mul(-gravity);
+//       physicsWorld.setGravity( normal );
+//     } else {
+//       physicsWorld.setGravity( new Ammo.btVector3(0, 0, 0) );
+//     }
 
-    frameCount++;
-    if (frameCount % 1 == 0) {
-      for (let i = 0; i < syncList.length; i++) { syncList[i](delta); }
-      physicsWorld.stepSimulation(delta, 10);
-    }
-  }
-  previousTruckSelected = truckSelected;
+//     frameCount++;
+//     if (frameCount % 1 == 0) {
+//       for (let i = 0; i < syncList.length; i++) { syncList[i](delta); }
+//       physicsWorld.stepSimulation(delta, 10);
+//     }
+//   }
+//   previousTruckSelected = truckSelected;
 }
 
 
