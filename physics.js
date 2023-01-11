c916a7fb5a6fafc42bfab8718698df8e851cd6f0
AsherSalomon <AsherSalomon@gmail.com>
2023-01-11 08:04:18 -0500

    Update physics.js

--- physics.js
+++ physics.js
@@ -375,7 +375,7 @@ function createVehicle(pos, quat) {
 //           // tm.setOrigin(position);
 //         }
         
-        theConsole.log(typeof positions[0]);
+        theConsole.log(Cesium.Cartographic.toCartesian(positions[0], ellipsoid));
 //         addPoint(cartesian3)
       }).catch(error => { throw error })
     }
