21db5ea7a9afa0168749b7d9ef31f4f5b54bf40e
AsherSalomon <AsherSalomon@gmail.com>
2023-01-16 07:56:33 -0500

    Update physics.js

--- physics.js
+++ physics.js
@@ -120,6 +120,7 @@ export function update(delta) {
   const ellipsoid = terrainProvider.tilingScheme.projection.ellipsoid;
   const cartographic = Cesium.Cartographic.fromCartesian(position, ellipsoid);
   if (cartographic.height < deadSeaElevation) {
+    console.log(cartographic.height, deadSeaElevation);
     hardReset = true;
     console.log('hardReset');
     viewer.trackedEntity = null;
