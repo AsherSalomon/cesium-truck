4d5a392854f5f6a403304e043a3e265e801b0ec3
AsherSalomon <AsherSalomon@gmail.com>
2023-01-16 07:31:42 -0500

    Update physics.js

--- physics.js
+++ physics.js
@@ -116,8 +116,7 @@ export function update(delta) {
   
   const deadSeaElevation = 430.5;
   const position = truckEntities[0].position.getValue(truckEntities.now());
-  console.log(Cesium.Cartesian3.magnitude(position));
-  if (Cesium.Cartesian3.magnitude(position) < 6378.1 - deadSeaElevation) {
+  if (Cesium.Cartesian3.magnitude(position) < 6378100 - deadSeaElevation) {
     hardReset = true;
     console.log('hardReset');
     viewer.trackedEntity = null;
