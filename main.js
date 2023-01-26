539f652161e9441bf4b37db945a8bf36123159a4
AsherSalomon <AsherSalomon@gmail.com>
2023-01-26 08:10:44 -0500

    Update main.js

--- main.js
+++ main.js
@@ -172,7 +172,8 @@ function update() {
     const crossProduct = new Cesium.Cartesian3();
     Cesium.Cartesian3.cross(viewer.camera.directionWC, vehicleDirection, crossProduct);
     const dotProduct = Cesium.Cartesian3.dot(viewer.camera.upWC, crossProduct);
-    viewer.camera.rotateRight(dotProduct * Math.PI / 256);
+    viewer.camera.rotateRight(dotProduct * Math.PI / 256); // to do, make camera follow at distance and tilt
+    viewer.camera.moveForward(0.001);
   }
 
   adjustHeightForTerrain(viewer.scene.screenSpaceCameraController);
