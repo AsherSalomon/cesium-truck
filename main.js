fe45945e4c843ba01a7a78fc97958ffbb062948b
AsherSalomon <AsherSalomon@gmail.com>
2023-01-27 11:53:22 -0500

    Update main.js

--- main.js
+++ main.js
@@ -92,19 +92,22 @@ document.body.onmouseup = function() {
 window.addEventListener('keydown', function(e) {
   followTruck = true;
 //   if (e.keyCode == 69) { // the E key
+  const key = e.keyCode;
+  if (key == 65 || key == 68 || key == 83 || key == 87) { // the E key
 //     if (viewer.trackedEntity == truckEntities[0]) {
 //       viewer.trackedEntity = null;
-//     } else if (viewer.trackedEntity != truckEntities[0]) {
-//       const matrix4 = new Cesium.Matrix4();
-//       Cesium.Transforms.eastNorthUpToFixedFrame(
-//         truckEntities[0].position._value, Cesium.Ellipsoid.WGS84, matrix4);
-//       Cesium.Matrix4.inverse(matrix4, matrix4);
-//       const position = new Cesium.Cartesian3();
-//       Cesium.Matrix4.multiplyByPoint(matrix4, viewer.camera.position, position);
-//       truckEntities[0].viewFrom = position;
-//       viewer.trackedEntity = truckEntities[0];
-//     }
-//   }
+//     } else 
+    if (viewer.trackedEntity != truckEntities[0]) {
+      const matrix4 = new Cesium.Matrix4();
+      Cesium.Transforms.eastNorthUpToFixedFrame(
+        truckEntities[0].position._value, Cesium.Ellipsoid.WGS84, matrix4);
+      Cesium.Matrix4.inverse(matrix4, matrix4);
+      const position = new Cesium.Cartesian3();
+      Cesium.Matrix4.multiplyByPoint(matrix4, viewer.camera.position, position);
+      truckEntities[0].viewFrom = position;
+      viewer.trackedEntity = truckEntities[0];
+    }
+  }
 });
 
 function adjustHeightForTerrain(controller) {
