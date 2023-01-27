12ddfe236aa575b4c69d7945f52baa6e2af60fca
AsherSalomon <AsherSalomon@gmail.com>
2023-01-27 12:33:59 -0500

    Update physics.js

--- physics.js
+++ physics.js
@@ -409,8 +409,8 @@ function createVehicle(pos, quat) {
 //       const restoreTorque = new Ammo.btVector3(crossProductSum.x, crossProductSum.y, crossProductSum.z);
 //       body.applyTorque(restoreTorque);
 //       Ammo.destroy(restoreTorque);
-      body.m_angularDamping = 0.001;
-      console.log(body.getAngularDamping());
+      
+      console.log(body.getLinearDamping(), body.getAngularDamping());
       
     }
     
