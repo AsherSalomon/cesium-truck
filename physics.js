67df381f8a1ab15b09bcb1b35e1b46bb3e286c94
AsherSalomon <AsherSalomon@gmail.com>
2023-01-30 07:48:29 -0500

    Update physics.js

--- physics.js
+++ physics.js
@@ -434,8 +434,8 @@ function createVehicle(pos, quat) {
       Cesium.Cartesian3.normalize(position, position);
       const crossProduct = new Cesium.Cartesian3();
       Cesium.Cartesian3.cross(aboveVehicle, position, crossProduct);
-      let resetTorqueValue = (Cesium.Cartesian3.angleBetween(aboveVehicle, position) - Math.PI / 4) * resetTorque / (Math.PI / 4);
-      if (resetTorqueValue < 0) { resetTorqueValue = 0; }
+      let resetTorqueValue = Cesium.Cartesian3.angleBetween(aboveVehicle, position) * resetTorque / (Math.PI / 4);
+//       if (resetTorqueValue < 0) { resetTorqueValue = 0; }
       if (resetTorqueValue > resetTorque) { resetTorqueValue = resetTorqueValue; }
       Cesium.Cartesian3.normalize(crossProduct, crossProduct);
       Cesium.Cartesian3.multiplyByScalar(crossProduct, resetTorqueValue, crossProduct);
