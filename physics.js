35bd2946ee261348a534a80717b206e702b3407b
AsherSalomon <AsherSalomon@gmail.com>
2023-01-30 07:20:47 -0500

    Update physics.js

--- physics.js
+++ physics.js
@@ -21,7 +21,7 @@ const gravity = 9.82;
 
 const resetTorque = 200000;
 const resetDamping = 1;
-const tippingAcceleration = gravity * 10;
+const tippingAcceleration = gravity * 100;
 
 let speedometer;
 const limiter = 85;//mph
@@ -317,7 +317,7 @@ function createVehicle(pos, quat) {
     let steeringSpeed = steeringIncrement * dt / 0.0167; // / Math.max(Math.abs(speed), 10);
     steeringClamp = Math.asin( 3.5 * tippingAcceleration / Math.abs(vehicle.getCurrentSpeedKmHour()) ** 2 );
     if (steeringClamp > Math.PI/6 || isNaN(steeringClamp)) { steeringClamp = Math.PI/6; }
-//     steeringSpeed = steeringClamp / 10;
+    steeringSpeed = steeringClamp / 10;
 //     if (actions.left) {
 //       if (vehicleSteering < steeringClamp)
 //         vehicleSteering += steeringSpeed;
