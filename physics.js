43bfb11b304e47dab61d68d7780ce68fb80f51e2
AsherSalomon <AsherSalomon@gmail.com>
2023-02-01 13:06:38 -0500

    Update physics.js

--- physics.js
+++ physics.js
@@ -131,7 +131,7 @@ export function update(delta) {
     viewer.trackedEntity = null;
   } else if(hardReset) {
     hardReset = false;
-    viewer.camera.lookUp(Math.PI/2);
+    viewer.camera.lookUp(Math.PI/4);
     viewer.trackedEntity = truckEntities[0];
   }
   
